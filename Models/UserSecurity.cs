20bf03450c96e28d163aa94c75edefa8154c1448
Nguyen pham do <0938224718nguyeb@gmail.com>
2022-05-01 10:21:26 +0700

    firstcommot

---
+++ Models/UserSecurity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace streaming_video_user.Models
+{
+    public partial class UserSecurity
+    {
+        public string IdUser { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public bool StatusDelete { get; set; }
+
+        public virtual User IdUserNavigation { get; set; } = null!;
+    }
+}
